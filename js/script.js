c47c8d5ef2bb44b52c3acedb0b98f817dc703509
cwhyman <cwhyma@gmail.com>
2021-07-11 14:51:01 +0100

    Added functionality to change div background color on mouseover

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const container = document.querySelector("#container");
 container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 
+//create a function to create the grid
 function createGrid(columns, rows) {
     for (let i=0; i< columns * rows; i++) {
         const squareDiv = document.createElement("div");
@@ -19,6 +20,24 @@ function createGrid(columns, rows) {
 }
 
 createGrid(columns, rows)
+
+//add event listeners to all squareDiv elements
+
+allDivs = document.querySelectorAll('.squareDiv');
+
+// we use the .forEach method to iterate through each button
+allDivs.forEach((div) => {
+
+  // and for each one we add a 'click' listener
+  div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'blue';
+  });
+});
+
+
+
+
+
 
 
 
