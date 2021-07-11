ed4bcd14249cf8e382ffcbe91713ad3e70bf2774
cwhyman <cwhyma@gmail.com>
2021-07-11 14:43:08 +0100

    Added initial grid

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//define the grid columns and rows
+columns = 16;
+rows = 16;
+
+//select the container div
+const container = document.querySelector("#container");
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+function createGrid(columns, rows) {
+    for (let i=0; i< columns * rows; i++) {
+        const squareDiv = document.createElement("div");
+        let id = i+1;
+        squareDiv.id = 'box' + id;
+        squareDiv.className = 'squareDiv';
+        squareDiv.textContent = id;
+        container.appendChild(squareDiv);    
+    }
+}
+
+createGrid(columns, rows)
+
+
+
